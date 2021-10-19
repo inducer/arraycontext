54ae38a75fc50affb211c2c7b13edb48ec963ccc
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-10-19 09:23:33 -0500

    define TaggableCLArray

--- arraycontext/impl/pytato/utils.py
+++ arraycontext/impl/pytato/utils.py
@@ -23,11 +23,87 @@ THE SOFTWARE.
 """
 
 
-from typing import Any, Dict, Set, Tuple, Mapping
-from pytato.array import SizeParam, Placeholder, make_placeholder
+from typing import Any, Dict, Set, Tuple, Mapping, FrozenSet, Optional
+from pytato.array import SizeParam, Placeholder, make_placeholder, Axis
 from pytato.array import Array, DataWrapper, DictOfNamedArrays
 from pytato.transform import CopyMapper
 from pytools import UniqueNameGenerator
+from pytools.tag import Taggable, Tag
+import pyopencl.array as cl_array
+
+
+class TaggableCLArray(cl_array.Array, Taggable):
+    """
+    A :class:`cl_array.Array` that can be tagged.
+    """
+    def __init__(self, cq, shape, dtype, order="C", allocator=None,
+                 data=None, offset=0, strides=None, events=None, _flags=None,
+                 _fast=False, _size=None, _context=None, _queue=None,
+                 axes=None, tags=frozenset()):
+
+        super().__init__(cq=cq, shape=shape, dtype=dtype,
+                         order=order, allocator=allocator,
+                         data=data, offset=offset,
+                         strides=strides, events=events,
+                         _flags=_flags, _fast=_fast,
+                         _size=_size, _context=_context,
+                         _queue=_queue)
+
+        self.tags = tags
+        self.axes = axes
+
+    def copy(self, queue=cl_array._copy_queue, tags=None, axes=None):
+        if tags is not None or axes is not None:
+            if queue is not cl_array._copy_queue:
+                raise ValueError("Cannot change both 'tags'/'axes' and 'queue'"
+                                 " at once.")
+            tags = self.tags if tags is None else tags
+            axes = self.axes if axes is None else axes
+            return self.__class__(None, self.shape, self.dtype,
+                                  allocator=self.allocator,
+                                  strides=self.strides, data=self.base_data,
+                                  offset=self.offset, events=self.events,
+                                  _fast=True, _context=self.context,
+                                  _queue=self.queue, _size=self.size,
+                                  tags=tags, axes=axes)
+        else:
+            new_with_queue = super().copy(queue=queue)
+            return self.__class__(None, new_with_queue.shape,
+                                  new_with_queue.dtype,
+                                  allocator=new_with_queue.allocator,
+                                  strides=new_with_queue.strides,
+                                  data=new_with_queue.base_data,
+                                  offset=new_with_queue.offset,
+                                  events=new_with_queue.events, _fast=True,
+                                  _context=new_with_queue.context,
+                                  _queue=queue, _size=new_with_queue.size,
+                                  tags=new_with_queue.tags,
+                                  axes=new_with_queue.axes)
+
+
+def to_tagged_cl_array(ary: cl_array.Array,
+                       axes: Optional[Tuple[Axis, ...]],
+                       tags: FrozenSet[Tag]) -> TaggableCLArray:
+    """
+    Converts a *ary* to a :class:`TaggableCLArray` with *tags* attached to it.
+
+    :arg axes: An instance of :class:`pytato.Axis` for each dimension of the
+        array. If passed *None*, then initialized to a :class:`pytato.Axis`
+        with no tags attached for each dimension.
+    """
+    axes = axes if axes is not None else tuple(Axis(frozenset())
+                                               for _ in ary.shape)
+
+    return TaggableCLArray(None, ary.shape,
+                           ary.dtype,
+                           allocator=ary.allocator,
+                           strides=ary.strides,
+                           data=ary.base_data,
+                           offset=ary.offset,
+                           events=ary.events, _fast=True,
+                           _context=ary.context,
+                           _queue=ary.queue, _size=ary.size,
+                           tags=tags)
 
 
 class _DatawrapperToBoundPlaceholderMapper(CopyMapper):
