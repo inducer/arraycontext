0fc02db04ab4065834200eb9c9f8eb718f3b69b5
Matthew Smith <mjsmith6@illinois.edu>
2024-06-13 15:43:45 -0500

    remove duplicates in _to_input_for_compiled

--- arraycontext/impl/pytato/compile.py
+++ arraycontext/impl/pytato/compile.py
@@ -182,7 +182,8 @@ def _to_input_for_compiled(
     """
     from arraycontext.impl.pyopencl.taggable_cl_array import to_tagged_cl_array
     if isinstance(ary, pt.Array):
-        dag = pt.make_dict_of_named_arrays({"_actx_out": ary})
+        dag = pt.transform.deduplicate(
+            pt.make_dict_of_named_arrays({"_actx_out": ary}))
         # Transform the DAG to give metadata inference a chance to do its job
         return actx.transform_dag(dag)["_actx_out"].expr
     elif isinstance(ary, TaggableCLArray):
