478fa3b79be366f944bedb4a34cb4a9c847b326c
Nick <koskelo2@illinois.edu>
2024-07-30 16:33:13 -0500

    Update imports to match the location.

--- test/test_pytato_parameter_study.py
+++ test/test_pytato_parameter_study.py
@@ -26,21 +26,15 @@ import logging
 
 import pytest
 
-from pytools.tag import Tag
-
 from arraycontext import (
-    PytatoPyOpenCLArrayContext,
     pytest_generate_tests_for_array_contexts,
 )
-from arraycontext.parameter_study.transform import (
-    ParamStudyPytatoPyOpenCLArrayContext,
-    ParameterStudyAxisTag
-)
 from arraycontext.parameter_study import (
-        pack_for_parameter_study,
-        unpack_parameter_study,
+    pack_for_parameter_study,
+    unpack_parameter_study,
+    ParameterStudyAxisTag,
+    ParamStudyPytatoPyOpenCLArrayContext,
 )
-
 from arraycontext.pytest import _PytestPytatoPyOpenCLArrayContextFactory
 
 
@@ -78,6 +72,7 @@ class ParamStudy1(ParameterStudyAxisTag):
     1st parameter study.
     """
 
+
 class ParamStudy2(ParameterStudyAxisTag):
     """
     2bd parameter study.
@@ -91,7 +86,6 @@ def test_pack_for_parameter_study(actx_factory):
 
     actx = actx_factory()
 
-    from arraycontext.impl.pytato import _BasePytatoArrayContext
     if not isinstance(actx, ParamStudyPytatoPyOpenCLArrayContext):
         pytest.skip("only parameter study array contexts are supported")
 
@@ -104,7 +98,6 @@ def test_pack_for_parameter_study(actx_factory):
     x1 = actx.from_numpy(rng.random(base_shape))
     x2 = actx.from_numpy(rng.random(base_shape))
     x3 = actx.from_numpy(rng.random(base_shape))
-    
 
     y0 = actx.from_numpy(rng.random(base_shape))
     y1 = actx.from_numpy(rng.random(base_shape))
@@ -112,15 +105,15 @@ def test_pack_for_parameter_study(actx_factory):
     y3 = actx.from_numpy(rng.random(base_shape))
     y4 = actx.from_numpy(rng.random(base_shape))
 
-    def rhs(a,b):
+    def rhs(a, b):
         return a + b
 
     # Adding to the end.
     pack_x = pack_for_parameter_study(actx, ParamStudy1, (4,), x0, x1, x2, x3)
-    assert pack_x.shape == (15,5,4)
+    assert pack_x.shape == (15, 5, 4)
 
-    pack_y = pack_for_parameter_study(actx, ParamStudy2, (5,), y0,y1, y2,y3,y4)
-    assert pack_y.shape == (15,5,5)
+    pack_y = pack_for_parameter_study(actx, ParamStudy2, (5,), y0, y1, y2, y3, y4)
+    assert pack_y.shape == (15, 5, 5)
 
     for i in range(3):
         axis_tags = pack_x.axes[i].tags_of_type(ParamStudy1)
@@ -131,11 +124,11 @@ def test_pack_for_parameter_study(actx_factory):
             assert not axis_tags
         assert not second_tags
 
+
 def test_unpack_parameter_study(actx_factory):
 
     actx = actx_factory()
 
-    from arraycontext.impl.pytato import _BasePytatoArrayContext
     if not isinstance(actx, ParamStudyPytatoPyOpenCLArrayContext):
         pytest.skip("only parameter study array contexts are supported")
 
@@ -148,7 +141,6 @@ def test_unpack_parameter_study(actx_factory):
     x1 = actx.from_numpy(rng.random(base_shape))
     x2 = actx.from_numpy(rng.random(base_shape))
     x3 = actx.from_numpy(rng.random(base_shape))
-    
 
     y0 = actx.from_numpy(rng.random(base_shape))
     y1 = actx.from_numpy(rng.random(base_shape))
@@ -156,37 +148,86 @@ def test_unpack_parameter_study(actx_factory):
     y3 = actx.from_numpy(rng.random(base_shape))
     y4 = actx.from_numpy(rng.random(base_shape))
 
-    def rhs(a,b):
+    def rhs(a, b):
         return a + b
 
     pack_x = pack_for_parameter_study(actx, ParamStudy1, (4,), x0, x1, x2, x3)
-    assert pack_x.shape == (15,5,4)
+    assert pack_x.shape == (15, 5, 4)
 
-    pack_y = pack_for_parameter_study(actx, ParamStudy2, (5,), y0,y1, y2,y3,y4)
-    assert pack_y.shape == (15,5,5)
+    pack_y = pack_for_parameter_study(actx, ParamStudy2, (5,), y0, y1, y2, y3, y4)
+    assert pack_y.shape == (15, 5, 5)
 
     compiled_rhs = actx.compile(rhs)
 
     output = compiled_rhs(pack_x, pack_y)
 
-    assert output.shape(15,5,4,5)
+    assert output.shape(15, 5, 4, 5)
 
     output_x = unpack_parameter_study(output, ParamStudy1)
     assert len(output_x) == 1  # Only 1 study associated with this variable.
-    assert len(output_x[0]) == 4 # 4 inputs for the parameter study.
+    assert len(output_x[0]) == 4  # 4 inputs for the parameter study.
     for i in range(len(output_x[0])):
         assert output_x[0][i].shape == (5, 15, 5)
 
-
     output_y = unpack_parameter_study(output, ParamStudy2)
     assert len(output_y) == 1  # Only 1 study associated with this variable.
-    assert len(output_y[0]) == 5 # 5 inputs for the parameter study.
+    assert len(output_y[0]) == 5  # 5 inputs for the parameter study.
     for i in range(len(output_y[0])):
         assert output_y[0][i].shape == (4, 15, 5)
 
 
-# }}}
+def test_one_time_step_advection(actx_factory):
 
+    actx = actx_factory()
+
+    if not isinstance(actx, ParamStudyPytatoPyOpenCLArrayContext):
+        pytest.skip("only parameter study array contexts are supported")
+
+    import numpy as np
+    seed = 12345
+    rng = np.random.default_rng(seed)
+
+    base_shape = np.prod((15, 5))
+    x0 = actx.from_numpy(rng.random(base_shape))
+    x1 = actx.from_numpy(rng.random(base_shape))
+    x2 = actx.from_numpy(rng.random(base_shape))
+    x3 = actx.from_numpy(rng.random(base_shape))
+
+    speed_shape = (1,)
+    y0 = actx.from_numpy(rng.random(speed_shape))
+    y1 = actx.from_numpy(rng.random(speed_shape))
+    y2 = actx.from_numpy(rng.random(speed_shape))
+    y3 = actx.from_numpy(rng.random(speed_shape))
+
+    ht = 0.0001
+    hx = 0.005
+    inds = actx.arange(base_shape)
+    kp1 = actx.roll(inds, -1)
+    km1 = actx.roll(inds, 1)
+
+    def rhs(fields, wave_speed):
+        # 2nd order in space finite difference
+        return fields + wave_speed * (-1) * (ht / (2 * hx)) * \
+                (fields[kp1] - fields[km1])
+
+    pack_x = pack_for_parameter_study(actx, ParamStudy1, (4,), x0, x1, x2, x3)
+    assert pack_x.shape == (75, 4)
+
+    pack_y = pack_for_parameter_study(actx, ParamStudy1, (4,), y0, y1, y2, y3)
+    assert pack_y.shape == (1, 4)
+
+    compiled_rhs = actx.compile(rhs)
+
+    output = compiled_rhs(pack_x, pack_y)
+
+    assert output.shape(75, 4)
+
+    output_x = unpack_parameter_study(output, ParamStudy1)
+    assert len(output_x) == 1  # Only 1 study associated with this variable.
+    assert len(output_x[0]) == 4  # 4 inputs for the parameter study.
+
+
+# }}}
 
 
 if __name__ == "__main__":
