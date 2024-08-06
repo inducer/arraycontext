2bcc9213d1f3d6d1cfda21d0b6c9da445eb1daac
Andreas Kloeckner <inform@tiker.net>
2024-08-05 19:31:02 -0500

    Switch to __array_ufunc__ in tests as a way to avoid numpy broadcasting

--- test/test_arraycontext.py
+++ test/test_arraycontext.py
@@ -133,7 +133,8 @@ class DOFArray:
         self.array_context = actx
         self.data = data
 
-    __array_priority__ = 10
+    # prevent numpy broadcasting
+    __array_ufunc__ = None
 
     def __bool__(self):
         if len(self) == 1 and self.data[0].size == 1:
@@ -218,6 +219,8 @@ class MyContainer:
     mass: Union[DOFArray, np.ndarray]
     momentum: np.ndarray
     enthalpy: Union[DOFArray, np.ndarray]
+
+    __array_ufunc__ = None
 
     @property
     def array_context(self):
@@ -1364,7 +1367,8 @@ def test_container_equality(actx_factory):
 class Foo:
     u: DOFArray
 
-    __array_priority__ = 1  # disallow numpy arithmetic to take precedence
+    # prevent numpy arithmetic from taking precedence
+    __array_ufunc__ = None
 
     @property
     def array_context(self):
