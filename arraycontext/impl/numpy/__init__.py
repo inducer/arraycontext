734193222f2cc1b87a7f9b496e4dd79c44f7e442
Andreas Kloeckner <inform@tiker.net>
2024-11-14 08:30:47 -0600

    Bring back numpy actx constructor docs

--- arraycontext/impl/numpy/__init__.py
+++ arraycontext/impl/numpy/__init__.py
@@ -63,6 +63,8 @@ class NumpyNonObjectArray(metaclass=NumpyNonObjectArrayMetaclass):
 class NumpyArrayContext(ArrayContext):
     """
     A :class:`ArrayContext` that uses :class:`numpy.ndarray` to represent arrays.
+
+    .. automethod:: __init__
     """
 
     _loopy_transform_cache: dict[lp.TranslationUnit, lp.ExecutorBase]
